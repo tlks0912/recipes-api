77dfc0e5fa9e5d0ec5280bf62739f8dc473c2210
benchen2019 <ben.chen2019@gmail.com>
2021-09-15 22:57:28 +0800

    recipe data model

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Recipe struct {
+	//swagger:ignore
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
+
 func main() {
    router := gin.Defailt()
    router.Run()
